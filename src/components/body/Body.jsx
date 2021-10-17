f0dcd2b345353dd0a6ad51a4f4bf3741b175aea4
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2021-10-17 18:28:22 +0700

    ganti body

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,15 @@
+import Carousel from './Carousel/Carousel'
+import Kategori from './Kategori/Kategori'
+import Produk from './Produk/Produk'
+
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Body
--- src/components/body/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Carousel} from './Carousel/Carousel'
-export {default as Kategori} from './Kategori/Kategori'
-export {default asProduk} from './Produk'
